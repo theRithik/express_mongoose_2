2c1ea9552c9597bf17264f0d613c610e779722df
rithik <115656594+theRithik@users.noreply.github.com>
2023-04-02 23:17:11 +0530

    Add files via upload

---
+++ Routes/products_route.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const route = express.Router()
+const app = express()
+const product = require('../model/products')
+
+
+route.get('/',(req,res)=>{
+    res.render('products.ejs')
+})
+
+route.post('/addProduct',(req,res)=>{
+    req.body.date = new Date();
+                product.create(req.body,(err,result)=>{
+                    if(err){
+                        console.log(err)
+                    
+                    }
+                    else{
+                        res.redirect('/products')
+                    }
+                })
+})
+
+route.get('/',(req,res)=>{
+    product.find((err,result)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.render('products.ejs', {productsData : result, form1:'add Products', form2:'view Products', form3:'update products', form4:'delete products'})
+        }
+    })
+})
+
+route.post('/delete',(req,res)=>{
+    product.findOneAndDelete({_id:req.body.id},(err,result)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.send('deleted the product sucessfully')
+        }
+    })
+})
+
+
+module.exports=route
